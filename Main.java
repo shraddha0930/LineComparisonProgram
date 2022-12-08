eae9563bc3bc9046b389a49efc0d672dbd1998de
shraddha0930 <shraddhaingole11@gmail.com>
2022-12-08 12:46:37 +0530

    pass welcome message

---
+++ Main.java
@@ -0,0 +1,8 @@
+package day3Practiceproblem;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("WELCOME to Line Comparison Computation Program");
+    }
+}
